ec9b13f02367eaaf554fd82b1808e71f13aa068f
Santosh Kuchetti <kuchettisantosh@gmail.com>
2023-02-18 12:16:24 +0530

    comments are added to the Sort Colors

--- twoPointers/SortColors.js
+++ twoPointers/SortColors.js
@@ -36,3 +36,18 @@ let req = function (arr) {
 }([2, 0, 2, 1, 1, 0]);
 
 console.log(req)
+
+/*
+    we can solve this problem using different approach
+    1 -> we can solve this problem using sorting. then all elements will be sorted and problem solved.
+    2 -> we can also solve the problem like counting the number of 1's, 2's, and 3's and create a new array
+     and add the values as the count of the 1's, 2's and 3's
+
+     but here it is required to use the same array to solve the problem. So whatever we need to do, we need to do within the array.
+     we can solve this problem using two pointer approch.
+     taking 1 pointer at start and another pointer at last. 
+     if the element is equal to 0, then we will swap the element with starting pointer and increment it
+     if the element is equal to 2, then we will swap the element with end pointer and decrement it and continue.
+
+     we need to repeat this process untill we reach the the end pointer that's it.
+*/
